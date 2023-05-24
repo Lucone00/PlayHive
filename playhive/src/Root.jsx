f4d0627454e4ab4da0325f996cf94b079c02c91c
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-24 14:48:29 +0200

    installed tailwindcss

---
+++ playhive/src/Root.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"
+
+const Root = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </>
+  );
+};
+
+
+export default Root
