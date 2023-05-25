30a654e72ce02d63f7dc5543280f2cb24ccf36ab
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-25 09:16:16 +0200

    Colori search e icone

--- playhive/src/pages/homepage/Navbar.jsx
+++ playhive/src/pages/homepage/Navbar.jsx
@@ -8,7 +8,7 @@ import SearchInput from './SearchInput'
 
 
 const navigation = [
-  { name: <IconHome className="h-6 w-6" aria-hidden="true"/>, href: '#', current: false },
+  { name: <IconHome className="h-6 w-6" aria-hidden="true"/>, href: '#', current: true },
   { name: <IconGroup className="h-6 w-6" aria-hidden="true"/>, href: '#', current: false },
   { name: <IconMessage className="h-6 w-6" aria-hidden="true"/>, href: '#', current: false },
   { name: <BellIcon className="h-6 w-6" aria-hidden="true" />, href: '#', current: false },
@@ -21,14 +21,14 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-slate-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -57,7 +57,7 @@ export default function Example() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'text-red-500' : 'text-gray-300 hover:bg-slate-800 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -67,7 +67,7 @@ export default function Example() {
                     ))}
                   </div>
                 </div>
-            <SearchInput/>
+                <SearchInput/>
               </div>
             </div>
           </div>
--- playhive/src/pages/homepage/SearchInput.jsx
+++ playhive/src/pages/homepage/SearchInput.jsx
@@ -1,13 +1,13 @@
 export default function SearchInput() {
     return (
         <div className="flex items-center">
-            <div className="flex space-x-1">
+            <div className="flex space-x-1 bg-slate-900 border border-transparent rounded-full">
                 <input
                     type="text"
-                    className="block w-full px-4 py-2 text-red-700 bg-white border rounded-full focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    className="block w-full px-4 py-2 text-slate-300 bg-slate-900 border border-transparent rounded-full rounded-r-none focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Search..."
                 />
-                <button className="px-4 text-white bg-red-600 rounded-full ">
+                <button className="px-4 text-white bg-red-600 rounded-full rounded-l-none">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="w-5 h-5"
