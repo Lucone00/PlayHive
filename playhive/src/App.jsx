a63a2614b4b4ba3ab445a4470d95fe14b3f73ea3
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-05-31 16:36:28 +0200

    character section title added

--- playhive/src/App.jsx
+++ playhive/src/App.jsx
@@ -1,10 +1,12 @@
 import ProfileBanner from "./components/ProfileBanner";
 import Error404 from './components/Error404.jsx'
+import NewPost from "./components/NewPost";
 function App() {
   return (
     <>
       <ProfileBanner />
       <Error404/>
+      <NewPost/>
     </>
   );
 }
--- playhive/src/components/ImageBox.jsx
+++ playhive/src/components/ImageBox.jsx
@@ -21,6 +21,7 @@ const ImageBox = () => {
 
   return (
     <div className="character-framework">
+      <h4>Character:</h4>
       <div className="image-box">
         <img
           src={selectedImage}
