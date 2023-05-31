cfebca32bab6fb58bd1acc1a075e89566097025d
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-31 09:16:25 +0200

    News Section, da completare le funzionalità

--- playhive/src/pages/homepage/HomePage.jsx
+++ playhive/src/pages/homepage/HomePage.jsx
@@ -1,10 +1,19 @@
 import Navbar from "./navbar/Navbar";
-import "./HomePage.css"
+import NewPost from "./newpost/NewPost";
+import NewsSection from "./news/NewsSection";
 
 function HomePage(){
 return <>
-        <div className="background">
+        <div className="bg-[#1D222D]">
                 <Navbar/>
+                <div className="flex gap-x-3 mx-6">
+                        <div className="max-w-sm py-4 px-8 bg-gray-800 shadow-lg rounded-lg my-20 text-slate-300">
+                                <h3>NEWS</h3>
+                                <hr />
+                                <NewsSection/>
+                                <NewsSection/>
+                        </div>
+                </div>
         </div>
         </>
 }
